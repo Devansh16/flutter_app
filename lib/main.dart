ecdd70e0461f50d9c54b4f9999183501c7955188
Devansh Chawla <devansh@vt.edu>
2020-06-28 19:03:23 +0530

    Made Changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,1 @@
-import 'package:flutter/material.dart'; // Includes a lot of stuff (buttons, etc)
 
-void main() {
-  runApp(MyApp()); // To execute the app
-}
-
-class MyApp extends StatelessWidget {
-  // Class "MyApp" inherits the StateLessWidget Class for its functionality.
-  Widget build(BuildContext context) {
-    // Class "build" return type "Widget"
-    return MaterialApp(
-      home: Text('Hello, World!'),
-    );
-  }
-}
