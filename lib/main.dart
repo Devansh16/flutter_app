ec2bf21a6085f1d47689f3d7d43cf06fe7c08a1e
Devansh Chawla <devansh@vt.edu>
2020-07-01 22:59:04 +0530

    Added Buttons in Column Widget

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,28 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      "What is your name?",
+      "What is your age?",
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Welcome'),
         ),
-        body: Text('Welcome to my App!'),
+        body: Column(
+          children: [
+            Text('Questions!'),
+            RaisedButton(
+              child: Text(questions[0]),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text(questions[1]),
+              onPressed: null,
+            ),
+          ],
+        ),
       ),
     );
   }
